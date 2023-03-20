a14324c6e1636331338abfd12dcf48847d1eb797
Hello-Disney <127477198+chanreddy800@users.noreply.github.com>
2023-03-20 21:33:25 +0530

    Update test_taskGithub.py

--- test_cases/test_taskGithub.py
+++ test_cases/test_taskGithub.py
@@ -7,7 +7,7 @@ import requests
 import datetime
 
 header = {
-    'Authorization': 'Bearer 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',
+    'Authorization': 'Bearer token',
     'Accept': 'application/vnd.github+json',
     'X-GitHub-Api-Version': '2022-11-28'
 }
